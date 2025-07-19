179231a48133a236c37bccc7a6317bb890c2bb81
Vishal Gupta <vishalgupta252310@gmail.com>
2025-07-19 20:41:07 +0530

    added daisy ui and nav bar also

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      
+      <div className="navbar bg-base-300 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">DevTinder</a>
+        </div>
+        <div className="flex gap-2">
+          <div className="dropdown dropdown-end mx-5">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Click Me
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
